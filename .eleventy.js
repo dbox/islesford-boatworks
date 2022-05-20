9eaa4b3ec97c3395b610e6e07d8cd7feab1f38c6
Daniel Box <danielbox@gmail.com>
2022-05-20 18:09:28 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ const htmlmin = require('html-minifier')
 
 module.exports = function (eleventyConfig) {
   // Copy static assets
-  eleventyConfig.addPassthroughCopy('src/static')
+  eleventyConfig.addPassthroughCopy('src/static');
+  
+  eleventyConfig.addPassthroughCopy('src/_redirects');
 
   // Minify HTML in production
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
